6c7e2988095640ead85d3d9f71ad0a241e809843
Greg Graham <greg@grahamtx.net>
2025-05-15 16:26:23 -0500

    Initial commit with answer scrambler

---
+++ scramble.py
@@ -0,0 +1,17 @@
+import random
+
+# Read the input file in.txt
+# The file should contain lines like:
+# 1) Answer 1
+with open("in.txt") as f:
+    lines = f.readlines()
+lines = [line.strip() for line in lines]
+answers = [line[3:] for line in lines]
+
+# Shuffle the answers
+random.shuffle(answers)
+
+# Write the scrambled answers to out.txt
+with open("out.txt", "w") as f:
+    for letter, answer in zip("ABCDE", answers):
+        f.write(f"    {letter}) {answer}\n")
